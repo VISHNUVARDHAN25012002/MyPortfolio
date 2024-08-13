346d1e8ae949d27037a301e1abfa9d0ed983bfc2
VISHNUVARDHAN25012002 <116712471+VISHNUVARDHAN25012002@users.noreply.github.com>
2024-08-13 12:25:48 +0530

    remove speed

--- src/App.js
+++ src/App.js
@@ -5,14 +5,12 @@ import Skills from "./components/Skills/skills";
 import Works from "./components/Works/works";
 import Contact from "./components/contact/contact";
 import Footer from "./components/Footer/footer";
-import { SpeedInsights } from '@vercel/speed-insights/react';  // Import SpeedInsights
+
 import { HashRouter } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      {/* Integrate SpeedInsights for performance monitoring */}
-      <SpeedInsights />
+    
 
       <HashRouter>
         <Navbar />
