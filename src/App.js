9d134fefb816ceba50af930324ba682ebb6cd567
VISHNUVARDHAN25012002 <116712471+VISHNUVARDHAN25012002@users.noreply.github.com>
2024-08-13 12:19:48 +0530

    speed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
+import React from 'react';
 import Navbar from "./components/NavBar/navbar";
 import Intro from "./components/Intro/intro";
 import Skills from "./components/Skills/skills";
 import Works from "./components/Works/works";
 import Contact from "./components/contact/contact";
 import Footer from "./components/Footer/footer";
-import {  HashRouter } from "react-router-dom";
+import { SpeedInsights } from '@vercel/speed-insights/react';  // Import SpeedInsights
+import { HashRouter } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
+      {/* Integrate SpeedInsights for performance monitoring */}
+      <SpeedInsights />
+
       <HashRouter>
-     <Navbar/>
-     <Intro/>  
-     <Skills/>
-     <Works/>
-     <Contact/>
-     <Footer/></HashRouter>
+        <Navbar />
+        <Intro />
+        <Skills />
+        <Works />
+        <Contact />
+        <Footer />
+      </HashRouter>
     </div>
   );
 }
